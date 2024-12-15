4e34b1f99331fc11d3b59b20750e0f150c4a6c76
Fernando Araujo <66099414+IIFernando@users.noreply.github.com>
2024-12-15 03:02:10 -0300

    Add files via upload
    Projeto Faculdade

---
+++ code/Enemy.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from time import sleep
+
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY, WIN_HEIGHT, WIN_WIDTH
+from code.EnemyShot import EnemyShot
+from code.Entity import Entity
+
+
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+        self.vertical_direction = 1
+        self.vertical_speed = ENTITY_SPEED[self.name]
+
+    def move(self):
+        if self.name == "Enemy3":
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+
+            self.rect.centery += self.vertical_direction * self.vertical_speed
+
+            if self.rect.top <= 0:
+                self.vertical_direction = 1
+                self.vertical_speed = ENTITY_SPEED[self.name] * 2
+
+            elif self.rect.bottom >= WIN_HEIGHT:
+                self.vertical_direction = -1
+                self.vertical_speed = ENTITY_SPEED[self.name]
+
+        else:
+            self.rect.centerx -= ENTITY_SPEED[self.name]
+
+    def shoot(self):
+        self.shot_delay -= 1
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
